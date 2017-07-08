e7eef4ab7c1e0e3174ee786be8636e1e4ae96f0b
yumi <yumi@howler.space>
2017-07-08 10:21:36 -0400

    favicon fix, borders

--- includes/head.php
+++ includes/head.php
@@ -7,4 +7,4 @@ if (!isset($_SESSION['pref_css']))
 <meta name="robots" content="noindex">
 <link rel="stylesheet" href=<?php echo CONFIG_COMMON_WEBPATH."css/style_suckless.css"?>>
 <link rel="stylesheet" href=<?php echo $_SESSION['stylesheet']?>>
-<link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico"/>
+<link rel="shortcut icon" type="image/x-icon" href=<?php echo CONFIG_COMMON_WEBPATH."img/favicon.ico"?>>
