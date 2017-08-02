778e7f13886d07b08da0813b50b1881d6d50fd28
yumi <yumi@howler.space>
2017-08-01 20:57:32 -0400

    Tidying login section

--- includes/accounts.php
+++ includes/accounts.php
@@ -14,15 +14,73 @@ function account_create($username, $password, $invited_by = NULL)
 	}
 	$salt = randomHex(16);
 	$hash = $password;
+	// crypt(3) formatted hash+salt
 	$hash = crypt($hash, '$6$'.$salt.'$');
 	$salted_password = $hash;
-	$query = "INSERT INTO `users` (username, password, invited_by) VALUES ('" . $username."','".$salted_password."','".$invited_by."')";
 	mysqli_query($dbh, "INSERT INTO `users`"
 	. " (`username`, `password`, `invited_by`) VALUES"
 	. " ('$username', '$salted_password', '$invited_by')");
+	mysqli_close($dbh);
 	return true;
 }
 function login_validate($username, $password)
 {
+	$dbh = mysqli_connect(CONFIG_DB_SERVER,
+		CONFIG_DB_USERNAME,
+		CONFIG_DB_PASSWORD,
+		CONFIG_DB_DATABASE);
+	$username = mysqli_escape_string($dbh, $username);
+	$password = mysqli_escape_string($dbh, $password);
+	$res = mysqli_query($dbh, "SELECT"
+	. " `id`, `username`, `password`, `failed_logins`, `last_ip`"
+	. ", `last_login`, `pref_css` FROM `users`"
+	. "  WHERE username='$username'");
+	$row = mysqli_fetch_assoc($res);
+	if (!$row) {
+		syslog(LOG_INFO|LOG_DAEMON, "Failed log-in attempt for"
+		. " invalid user $username from " . $_SERVER['REMOTE_ADDR']);
+		return false;
+	}
+
+	if (!password_verify($password, $row['password'])) {
+		// Update the number of failed logins
+		$row['failed_logins']++;
+		$cmd = "UPDATE `onsen` SET `failed_logins`='$failed_logins' WHERE `username`='$sql_username'";
+		mysqli_query($dbh, "UPDATE `users` SET"
+		. " `failed_logins`=$failed_logins"
+		. " WHERE `username`='$username'");
+
+		syslog(LOG_INFO|LOG_DAEMON, "Failed log-in attempt for" 
+		. " $username from " . $_SERVER['REMOTE_ADDR']);
+		return false;
+	}
+
+	syslog(LOG_INFO|LOG_DAEMON, "Successful login for"
+	. " $username from " . $_SERVER['REMOTE_ADDR']);
+
+
+	// Initialize the user's session
+	$_SESSION['userid'] = $row['id'];
+	$_SESSION['username'] = $username;
+
+	// Reset the number of failed login attempts
+	mysqli_query("UPDATE `users` SET `failed_logins`=0"
+	. " WHERE `id`=" . $_SESSION['userid']);
+
+	// Update the styling sheets for our session
+	$_SESSION['pref_css'] = $row['pref_css'];
+	reloaduserstyle();
+
+	// Update 'last login' information to the current session
+	$today = date("Y-m-d H:i:s");
+	mysqli_query("UPDATE `users` SET `last_login`='$today'"
+	. ", `last_ip`='" . $_SERVER['REMOTE_ADDR'] . "'"
+	. " WHERE `id` = " . $_SESSION['userid']);
+
+	return [
+		'last_login' => $row['last_login'],
+		'last_ip' => $row['last_ip'],
+		'failed_logins' => $row['failed_logins'],
+	];
 }
 ?>
--- login.php
+++ login.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <?php
 include "includes/core.php";
+include "includes/accounts.php";
 
 // Was the user refered by some other link?
 if (isset($_GET['ref']))
@@ -23,95 +24,34 @@ if (isset($_GET['ref']))
 		<a href="util/acc_create.php">create an account</a>
 	</header>
 <?php
-if (isset($_POST['onsen_username'], $_POST['onsen_password'])) {
+if (isset($_POST['username'], $_POST['password'])) {
 	// Don't login too fast!
 	sleep(5);
 
-	if (strlen($_POST['onsen_username']) > 20 || strlen($_POST['onsen_password']) > 50) { print '<script type="text/javascript">setTimeout(function () {window.history.back();}, 2000);</script>';return; }
-
-	$onsen_username = filter_var($_POST['onsen_username'], FILTER_SANITIZE_STRING);
-	$onsen_password = filter_var($_POST['onsen_password'], FILTER_SANITIZE_STRING);
-
-	$conn = new mysqli(CONFIG_DB_SERVER, CONFIG_DB_USERNAME, CONFIG_DB_PASSWORD, CONFIG_DB_DATABASE);
-	if ($conn->connect_error) {
-		syslog(LOG_INFO|LOG_DAEMON, "Could not connect to SQL database for user ".$onsen_username);
-		print "It looks like the SQL database is offline";
-		print '<script type="text/javascript">setTimeout(function () {window.history.back();}, 2000);</script>';
-		return;
+	print "<p>";
+	if ($info = login_validate($_POST['username'], $_POST['password'])) {
+		print "Welcome home " . $_SESSION['username']
+		. "<br/>"
+		. "You last logged in on " . $info['last_login']
+		. " from " . $info['last_ip'];
+		if ($info['failed_logins'] > 0) {
+			print "<br/>"
+			. "There were " . $info['failed_logins']
+			. " failed logins since you last logged in";
+		}
+		print "</p>"
+		. "<span><a href='";
+		if (isset($_GET['ref']))
+			print rawurldecode($_GET['ref']);
+		else
+			print CONFIG_WEBHOMEPAGE;
+		print "'>Continue</a></span>";
+		die;
 	}
-
-	// First check if the account you're trying to log in to is locked
-	$cmd = "SELECT `id`, `username`, `password`, `failed_logins`, `last_login`, `locked`, `pref_css` FROM `users` WHERE `username`='$onsen_username'";
-	$result=$conn->query($cmd);
-	if (!$result) {
-		syslog(LOG_INFO|LOG_DAEMON, "Could not query SQL database for user ".$onsen_username);
-		print "Something went wrong with your request. . . email someone who can fix it";
-		print '<script type="text/javascript">setTimeout(function () {window.history.back();}, 2000);</script>';
-		return;
+	else {
+		print "Invalid login attempt!";
 	}
-	$row = $result->fetch_assoc();
-	$sql_id = $row["id"];
-	$sql_username = $row["username"];
-	$sql_password = $row["password"];
-	$sql_failed_logins = $row["failed_logins"];
-	$sql_locked = $row["locked"];
-	$sql_last_login = $row["last_login"];
-	$sql_pref_css = $row["pref_css"];
-
-	// Handling incorrect usernames
-	if ($result->num_rows === 0) {
-		syslog(LOG_INFO|LOG_DAEMON, "Failed log-in attempt for non-existent user ".$onsen_username." from ".$_SERVER['REMOTE_ADDR']);
-		print "I cannot find an account like that!";
-		print '<script type="text/javascript">setTimeout(function () {window.history.back();}, 2000);</script>';
-		return;
-	}
-	// Handling locked accounts
-	if ($sql_locked == "y") {
-		syslog(LOG_INFO|LOG_DAEMON, "Failed log-in attempt for locked user ".$onsen_username." from ".$_SERVER['REMOTE_ADDR']);
-		print "Your account is locked, please try again in a few minutes~";
-		print '<script type="text/javascript">setTimeout(function () {window.history.back();}, 2000);</script>';
-		return;
-	}
-	// Handling incorrect passwords
-
-	$salt = substr($sql_password, 3-mb_strlen($sql_password), -129);
-	if (!password_verify($onsen_password, $sql_password)) {
-		syslog(LOG_INFO|LOG_DAEMON, "Failed log-in attempt for ".$onsen_username." from ".$_SERVER['REMOTE_ADDR']);
-		$failed_logins = $row["failed_logins"]+1;
-		print "You have failed to log in $failed_logins times";
-		$cmd = "UPDATE `onsen` SET `failed_logins`='$failed_logins' WHERE `username`='$sql_username'";
-		$conn->query($cmd);
-		print '<script type="text/javascript">setTimeout(function () {window.history.back();}, 2000);</script>';
-		return;
-	}
-
-	syslog(LOG_INFO|LOG_DAEMON, "Successful login for ".$sql_username." from ". $_SERVER['REMOTE_ADDR']);
-	// Set the number of failed logins to 0
-	$cmd = "UPDATE `onsen` SET `failed_logins`=0 WHERE `username`='$sql_username'";
-	$conn->query($cmd);
-
-	// Set preferred css style
-	$_SESSION['pref_css'] = $sql_pref_css;
-	reloadUserStyle();
-
-	// Set the last_login date to today
-	$today = date("Y-m-d H:i:s");
-	$cmd = "UPDATE `users` SET `last_login`='$today' WHERE `username`='$sql_username'";
-	$conn->query($cmd);
-
-	// Set the User ID, effectively logging the user in
-	$_SESSION['userid'] = $sql_id;
-	$_SESSION['username'] = $sql_username;
-	print "Welcome home $sql_username!</br>You last logged in on $sql_last_login";
-	if ($sql_failed_logins > 0)
-		print "</br>There were ".$sql_failed_logins." failed login attempts since the last successful login";
-	print '<script type="text/javascript">setTimeout(function () {window.location.href = "';
-	if (isset($_GET['ref']))
-		print rawurldecode($_GET['ref']);
-	else
-		print CONFIG_WEBHOMEPAGE;
-	print '";}, 2000);</script>';
-	die;	
+	print "</p>";
 }
 ?>
 
@@ -121,10 +61,10 @@ Please log in ～
 <?php
 echo('<form action="login.php'.$uri.'" method="post">');
 ?>
-	<label for="onsen_username">Username</label></br>
-	<input type="text" id="onsen_username" name="onsen_username" value="" maxlength="20" /></br>
-	<label for="onsen_password">Password</label></br>
-	<input type="password" id="onsen_password" name="onsen_password" maxlength="50" /></br></br>
+	<label for="username">Username</label></br>
+	<input type="text" name="username" value="" maxlength="20" /></br>
+	<label for="password">Password</label></br>
+	<input type="password" name="password" maxlength="50" /></br></br>
 	<input type="submit" value="Login»" />
 </form>
 </div>
